675995ea7d48d07c32412c0d3f09e437b77fb273
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-02-01 10:58:31 -0800

    finished footer - need to adjust the twitter and facebook icon

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './header';
 import Welcome from './welcomeImage';
 import Home from './welcomeInfo';
+import Footer from './footer';
 
 const App = () => (
     <div className="container">
         <Header />
         <Welcome />
         <Home />
+        <Footer />
     </div>
 );
 
