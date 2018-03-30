6736e819fa485d45ff99ad90571838aca2085344
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-03-30 12:32:21 -0700

    adding basic skeleton for gifts-page

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,7 @@ import Header from './header';
 import HomePage from './home_page';
 import ContactPage from './contact_page';
 import OurMacarons from './our-macarons';
+import GiftsParties from './gifts_page';
 import Footer from './footer';
 import {BrowserRouter as Router, Route, Navlink} from 'react-router-dom';
 
@@ -13,7 +14,7 @@ const App = () => (
         <Header />
         <Route exact path="/" component={HomePage}></Route>
         <Route path="/our-macarons" component={OurMacarons}></Route>
-        {/*<Route path="/gifts_parties" component={GiftsParties}></Route>*/}
+        <Route path="/gifts_parties" component={GiftsParties}></Route>
         <Route path="/contact_info" component={ContactPage}></Route>
         <Footer />
     </div>
---
+++ src/components/gifts_page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GiftsBanner from './gifts-banner';
+import GiftsContent from './gifts-content';
+
+const GiftsPage = () => {
+    return (
+        <div>
+           <GiftsBanner />
+           <GiftsContent />
+        </div>
+    )
+}
+
+export default GiftsPage;
